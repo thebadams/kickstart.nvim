01e4a48f3ef4c1aaf42eaa03db7dd88b0d2cc68d
thebadams <bpeteradams@gmail.com>
2024-11-27 17:25:32 -0500

    Refactor(plugins): todo-comments

--- init.lua
+++ init.lua
@@ -66,7 +66,6 @@ require('lazy').setup({
   -- LSP Plugins
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
