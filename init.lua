c76981e234a4ab203a1c1363fe8b7e20155001cb
thebadams <bpeteradams@gmail.com>
2024-11-27 16:43:21 -0500

    Refactor(autocommands): created autocommands module and moved highlight on yank autocommand to that file

--- init.lua
+++ init.lua
@@ -1,19 +1,6 @@
 require 'vim-options'
 require 'keymaps'
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+require 'autocommands'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
