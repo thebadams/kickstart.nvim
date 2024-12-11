575734dee6af32ffa76a70007d8918b68b077b01
thebadams <bpeteradams@gmail.com>
2024-12-10 21:45:12 -0500

    Added a dashboard plugin

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+-- lazy.nvim
+return {
+  'folke/snacks.nvim',
+  opts = {
+    dashboard = {
+      -- your dashboard configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}
